3f94d7dd514b5570e1664568ff5feb716343a505
Hien Giang <quachtrieuvu@gmail.com>
2023-12-12 23:29:31 +0700

    added function exercise

--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -21,37 +21,38 @@ import "fmt"
 
 //* Write a function that accepts a person's name as a function
 //  parameter and displays a greeting to that person.
-func greet (name string) {
-	fmt.Println("Hello ", name);
+func greetPerson (name string) {
+	fmt.Println("Hello", name);
 }
 
 //* Write a function that returns any message, and call it from within
 //  fmt.Println()
-func linda () string {
+func hiThere () string {
 	return "Á, chết chị rồi em ơi!";
 }
 
 //* Write a function to add 3 numbers together, supplied as arguments, and
 //  return the answer
-func add(a, b, c int) int {
+func addThree(a, b, c int) int {
 	return a + b + c;
 }
 //* Write a function that returns any number
-func returnNum () {
-	return 1;
+func five () int{
+	return 5;
 }
 //* Write a function that returns any two numbers
-func returnTwoNum () {
-	return 1, 2;
+func twoTwos () (int, int){
+	return 2, 2;
 }
 
 func main() {
-	fmt.Println(linda());
-
-
-
-
-//* Add three numbers together using any combination of the existing functions.
-//  * Print the result
 //* Call every function at least once
+	greetPerson("Hien");
+	fmt.Println(hiThere());
+//* Add three numbers together using any combination of the existing functions.
+ 	a, b := twoTwos();
+ 	res := addThree(a, b, five())
+//  * Print the result
+	fmt.Println("res =", res);
+
 }
